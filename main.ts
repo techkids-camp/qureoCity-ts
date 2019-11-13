ab75091627e8fc8bf660f70b56f47141f1edd407
ayutaz <ka1357amnbpdr@gmail.com>
2019-11-13 21:02:00 +0900

    make A class

--- main.ts
+++ main.ts
@@ -1,16 +1,5 @@
-// 将棋のゲーム作成
-function Start () {
-    playerX = player.position().getValue(0)
-    playerY = player.position().getValue(1)
-    playerZ = player.position().getValue(2)
-    // 将棋の盤の作成
-    blocks.fill(
-        Block.PlanksBirch,
-        positions.createWorld(playerX - 4, playerY - 1, playerZ),
-        positions.createWorld(playerX + 3, playerY - 1, playerZ + 8),
-        FillOperation.Replace
-    )
-}
-let playerX
-let playerY
-let playerZ
+class A{
+    hogeA(){
+        player.say("hogeA")
+    }
+} 
