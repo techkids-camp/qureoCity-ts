7b9452528a4db14396ef1a8851c22b927b522206
ayutaz <ka1357amnbpdr@gmail.com>
2019-11-13 21:18:59 +0900

    関数名の名前がかぶっていたので，修正

--- main.ts
+++ main.ts
@@ -2,20 +2,8 @@ class A{
     public hogeA(){
         player.say("hogeA")
     }
-    public hogeB(){
-        player.say("hogeB")
-    }
-    public hogeB(){
-        player.say("hogeB")
-    }
 }
 class B{
-    public hogeA(){
-        player.say("hogeA")
-    }
-    public hogeB(){
-        player.say("hogeB")
-    }
     public hogeB(){
         player.say("hogeB")
     }
