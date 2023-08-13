7839cc6f5938a08a7f540d10940332cc2301b655
akahoshi1421 <akaho1421@gmail.com>
2023-08-13 15:34:08 +0900

    テスト

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
-class A{
-    public hogeA(){
-        player.say("hogeA")
+//%  block="Artemis: Moon Journey" weight=200 color=#0B3D91 icon="\uf186"
+namespace qureoCity{
+    /**
+     * Building Blocks (Activity 5)
+     * Player 1
+     */
+    //% block="`Generics.blastOff` go for launch"
+    export function ずっと(func: () => void) {
+        func()
     }
 }
-class B{
-    public hogeB(){
-        player.say("hogeB")
-    }
-} 
